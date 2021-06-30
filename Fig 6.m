9f5d49bd49a569b811a90c4abb55acb05bbddfb2
Hamid Reza Ashkiyan <HamidRezaAshkiyan@gmail.com>
2021-06-30 17:24:35 +0330

    Finish Fig 6

--- Fig 6.m
+++ Fig 6.m
@@ -1,15 +1,26 @@
 # Fig 6
-x = [0:10:100];
-y = [1,1,0,1,0,1,0,1,0,1,1];
-y1 = [0,0,0,0,0,0,0,0,0,1,1];
+x = [0:5:100];
+#y = [1,1,0,1,0,1,0,1,0,1,1];
+#y1 = [0,0,0,0,0,0,0,0,0,1,1];
 
-figure
-plot(x,y, 
-      x, y1,'o-', 
-      'Color','m')
+y = [1,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1];
+y1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1];
 
-title ("Fig. 6: Output Health State membership function");
-xlabel ("Output Health State");
-ylim([-0.1 1.1])
+plot( x,  y, 
+      x,  y1, '-', "color", "r")
 
-grid on
+title ("Fig. 6: Output Health State membership function", 
+        "FontSize", 24 );
+xlabel ("Output Health State", 
+        "FontSize", 20 );
+        
+ylim([-0.1  1.2]);
+xlim([0    100]);
+yticks([0,0.5,1]);
+
+text (10, 1.1,  "Checkup",      "FontSize", 16);
+text (30, 1.1,  "Unwell",       "FontSize", 16);
+text (50, 1.1,  "Hypothermia",  "FontSize", 16);
+text (70, 1.1,  "Fever",        "FontSize", 16);
+text (90, 1.1,  "Healthy",      "FontSize", 16);
+0
